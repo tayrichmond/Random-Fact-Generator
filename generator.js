00eb37c2dc3d72ff400c0ef83ae41f3b7a163a2d
Taylor Richmond <taylorichmond@gmail.com>
2015-06-08 16:30:45 -0700

    Message changes every time button is clicked.

--- generator.js
+++ generator.js
@@ -9,10 +9,9 @@ var randomFacts = ['Odontophobia is the fear of teeth.',
   'Karoke means "empty orchestra" in Japanese.',
   "Elephants are the only mammals that can't jump."];
 
-var fact = randomString(randomFacts);
 
 $(function() {
-  $('button').click(function() {
-    $('div').html(fact);
+  $('button').on('click', function() {
+    $('div').text(randomString(randomFacts));
   });
 });
